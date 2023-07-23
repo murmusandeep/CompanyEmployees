7137f0aee45a63e391245250d950b3aff80068dd
Sandeep Murmu <murmusandeep17@gmail.com>
2023-07-23 22:48:38 +0530

    chapter 6 complete

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,6 +1,8 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities.Exceptions;
 using Service.Contracts;
+using Shared;
 
 namespace Service
 {
@@ -16,5 +18,26 @@ namespace Service
             _mapper = mapper;
         }
 
+        public IEnumerable<EmployeeDto> GetEmployees(Guid companyId, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+            var employeesFromDb = _repository.Employee.GetEmployees(companyId, trackChanges);
+            var employeesDto = _mapper.Map<IEnumerable<EmployeeDto>>(employeesFromDb);
+            return employeesDto;
+        }
+
+        public EmployeeDto GetEmployee(Guid companyId, Guid id, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+            var employeeDb = _repository.Employee.GetEmployee(companyId, id, trackChanges);
+            if (employeeDb is null)
+                throw new EmployeeNotFoundException(id);
+            var employee = _mapper.Map<EmployeeDto>(employeeDb);
+            return employee;
+        }
     }
 }
