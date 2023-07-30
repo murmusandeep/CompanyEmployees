9b63e29cfc259d1787e116d3227be78fd45c3533
Sandeep Murmu <murmusandeep17@gmail.com>
2023-07-30 11:31:07 +0530

    chapter 9 complete

--- Service/EmployeeService.cs
+++ Service/EmployeeService.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Contracts;
+using Entities;
 using Entities.Exceptions;
 using Service.Contracts;
 using Shared;
@@ -39,5 +40,18 @@ namespace Service
             var employee = _mapper.Map<EmployeeDto>(employeeDb);
             return employee;
         }
+
+        public EmployeeDto CreateEmployeeForCompany(Guid companyId, EmployeeForCreationDto employeeForCreation, bool trackChanges)
+        {
+            var company = _repository.Company.GetCompany(companyId, trackChanges);
+            if (company is null)
+                throw new CompanyNotFoundException(companyId);
+            var employeeEntity = _mapper.Map<Employee>(employeeForCreation);
+            _repository.Employee.CreateEmployeeForCompany(companyId, employeeEntity);
+            _repository.Save();
+            var employeeToReturn = _mapper.Map<EmployeeDto>(employeeEntity);
+            return employeeToReturn;
+        }
+
     }
 }
