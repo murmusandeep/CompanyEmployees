32777d468748502aedd260b2180353219a280487
Sandeep Murmu <murmusandeep17@gmail.com>
2023-07-30 13:56:55 +0530

    chapter 11 complete

--- Repository/RepositoryBase.cs
+++ Repository/RepositoryBase.cs
@@ -11,18 +11,12 @@ namespace Repository
             => RepositoryContext = repositoryContext;
 
         public IQueryable<T> FindAll(bool trackChanges) =>
-            !trackChanges ?
-            RepositoryContext.Set<T>()
-            .AsNoTracking() :
+            !trackChanges ? RepositoryContext.Set<T>().AsNoTracking() :
             RepositoryContext.Set<T>();
         public IQueryable<T> FindByCondition(Expression<Func<T, bool>> expression,
             bool trackChanges) =>
-            !trackChanges ?
-            RepositoryContext.Set<T>()
-            .Where(expression)
-            .AsNoTracking() :
-            RepositoryContext.Set<T>()
-            .Where(expression);
+            !trackChanges ? RepositoryContext.Set<T>().Where(expression).AsNoTracking() :
+            RepositoryContext.Set<T>().Where(expression);
         public void Create(T entity) => RepositoryContext.Set<T>().Add(entity);
         public void Update(T entity) => RepositoryContext.Set<T>().Update(entity);
         public void Delete(T entity) => RepositoryContext.Set<T>().Remove(entity);
