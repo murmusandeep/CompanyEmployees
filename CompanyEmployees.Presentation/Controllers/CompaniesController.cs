d66c91571440678af3197213d53da4f7bc27f917
Sandeep <murmusandeep17@gmail.com>
2025-08-16 13:36:27 +0530

    Chapter 13 completed

--- CompanyEmployees.Presentation/Controllers/CompaniesController.cs
+++ CompanyEmployees.Presentation/Controllers/CompaniesController.cs
@@ -31,6 +31,8 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (company is null)
                 return BadRequest("CompanyForCreationDto object is null");
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
             var createdCompany = _service.CompanyService.CreateCompany(company);
             return CreatedAtRoute("CompanyById", new { companyId = createdCompany.Id }, createdCompany);
         }
@@ -61,6 +63,8 @@ namespace CompanyEmployees.Presentation.Controllers
         {
             if (company is null)
                 return BadRequest("CompanyForUpdateDto object is null");
+            if (!ModelState.IsValid)
+                return UnprocessableEntity(ModelState);
             _service.CompanyService.UpdateCompany(id, company, trackChanges: true);
             return NoContent();
         }
